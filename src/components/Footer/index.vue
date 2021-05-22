1a16ee69f2d94f06e81971ab83403d93b866adc2
SBagaria2710 <shashwawtforwork@gmail.com>
2021-05-23 00:23:25 +0530

    initial project setup

---
+++ src/components/Footer/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer class="footer-container">
+      <h1>Footer Goes Here</h1>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+};
+</script>
+
+<style scoped>
+.footer-container {
+    padding: 16px;
+    height: 200px;
+    background-color: #efefef;
+}
+</style>
---
+++ src/components/Header/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <header>
+    <ul>
+      <li>
+        <a href="#" class="logo">
+          <img
+            width="35"
+            height="35"
+            src="@/../public/images/rds-logo.png"
+            alt="Real Dev Squad Logo"
+          />
+          RDS - Learn Site
+        </a>
+      </li>
+    </ul>
+  </header>
+</template>
+
+<script>
+
+export default {
+  name: 'Header',
+};
+</script>
+<style scoped>
+ul {
+  list-style: none;
+  display: flex;
+  justify-content: left;
+  background-color: #1d1283;
+  padding: 0;
+  margin: 0 0 1.5rem 0;
+}
+ul li a {
+  padding: 14px 16px;
+  display: flex;
+  align-items: center;
+  color: #fff;
+  text-decoration: none;
+}
+ul li a img{
+  margin-right: 8px;
+}
+.logo {
+    font-weight: 600;
+}
+ul li a:hover {
+  background-color: #fff3;
+}
+ul li a.active {
+  color: #87d870;
+}
+</style>
