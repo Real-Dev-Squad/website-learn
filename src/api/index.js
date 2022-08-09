987522d3919322362e8b8da5caff93e9404493c6
aakankshadhurandhar <aakankshadhurandhar34@gmail.com>
2022-08-09 13:20:45 +0530

    fix:merge

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,10 @@
-import axios from "axios";
+import axios from 'axios';
 
-const httpClient = axios.create( {
-  baseURL : process.env.BASE_URL,
-  headers : {
-    "Content-Type" : "application/json",
+const httpClient = axios.create({
+  baseURL: process.env.BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
   },
-} );
+});
 
 export default httpClient;
--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,12 @@
 /* eslint-disable no-undef */
-import { mount } from "@vue/test-utils";
-import Home from "@/App.vue";
+import { mount } from '@vue/test-utils';
+import Home from '@/App.vue';
 
+describe('Footer.vue', () => {
+  it('renders a footer', () => {
+    const wrapper = mount(Home);
 
-describe( "Footer.vue", () => {
-  it( "renders a footer", () => {
-    const wrapper = mount( Home  );
-
-    console.log( wrapper.html() );
-  } );
-} );
+    // eslint-disable-next-line no-console
+    console.log(wrapper.html());
+  });
+});
