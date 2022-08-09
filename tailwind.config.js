f2c987bfe88be2ba8a0cbf7c9086b08d0fda8ba5
Samarpan Harit <samarpan.harit@unicommerce.com>
2022-08-09 20:11:17 +0530

    feat: Tailwind setup (#72)

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    './public/index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+  ],
+  purge: [],
+  darkMode: 'media',
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
