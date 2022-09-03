8f3fb7857739670beba1334decc9eb6cfecbae63
Samarpan Harit <haritsamarpan7@gmail.com>
2022-09-03 22:34:25 +0530

    Added options for the Tooltip component

--- src/components/common/Tooltip/index.vue
+++ src/components/common/Tooltip/index.vue
@@ -1,21 +1,30 @@
 <template>
   <div
-    :class="`tooltip w-max h-max relative ${classes}`"
-    :data-tooltip="content"
-    :data-tooltip-position="position"
+    class="relative cursor-pointer w-max h-max"
+    :onmouseenter="trigger === 'hover' ? showTooltip : null"
+    :onmouseleave="trigger === 'hover' ? hideTooltip : null"
+    :onclick="trigger === 'click' ? toggleTooltip : null"
   >
-    <slot />
+  <div v-if="isTooltipVisible" :class="twMerge(`absolute p-1 px-3 text-sm transition-opacity animate-fade z-10 text-${TOOLTIP_TEXT_COLOR} bg-${TOOLTIP_BG_COLOR} rounded w-max text-left`, contentPositionalClasses[position])">
+    <!-- Arrow -->
+    <div v-if="!hideArrow" :class="twMerge('absolute', arrowPositionalClasses[position])"></div>
+    <slot name="content"></slot>
   </div>
+  <slot></slot>
+</div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
+import { twMerge } from 'tailwind-merge'
 
-defineProps({
-  content: String,
-  classes: String,
-  bgColor: {
-    type: String,
-    default: 'rgb(229 231 235)',
+const isTooltipVisible=ref(false);
+const ARROW_SIZE="0.375rem";
+
+const props = defineProps({
+  hideArrow:{
+    type: Boolean,
+    default: false
   },
   position: {
     type: String,
@@ -24,60 +33,71 @@ defineProps({
       return ['top', 'bottom', 'left', 'right'].includes(value);
     },
   },
+  trigger: {
+    type: String,
+    default: 'hover',
+    validator(value) {
+      return ['hover', 'click'].includes(value);
+    },
+  },
+  type:{
+    type: String,
+    default: 'dark',
+    validator(value) {
+      return ["dark","success","warning","error","secondary"].includes(value);
+    },
+  }
 });
+
+const TOOLTIP_TYPES = {
+  "dark":{
+    TOOLTIP_BG_COLOR:"black",
+    TOOLTIP_TEXT_COLOR:"white"
+  },
+  "success":{
+    TOOLTIP_BG_COLOR:"blue-700",
+    TOOLTIP_TEXT_COLOR:"white"
+  },
+  "warning":{
+    TOOLTIP_BG_COLOR:"yellow-500",
+    TOOLTIP_TEXT_COLOR:"white"
+  },
+  "error":{
+    TOOLTIP_BG_COLOR:"red-600",
+    TOOLTIP_TEXT_COLOR:"white"
+  },
+  "secondary":{
+    TOOLTIP_BG_COLOR:"neutral-600",
+    TOOLTIP_TEXT_COLOR:"white"
+  },
+}
+
+const {TOOLTIP_BG_COLOR, TOOLTIP_TEXT_COLOR} = TOOLTIP_TYPES[props.type];
+
+const contentPositionalClasses = {
+  "left": `right-full bottom-1/2 translate-y-1/2 ${props.hideArrow? 'mr-1':'mr-2.5'}`,
+  "right": `left-full bottom-1/2 translate-y-1/2 ${props.hideArrow? 'ml-1':'ml-2.5'}`, 
+  "top": `bottom-full left-1/2 -translate-x-1/2 ${props.hideArrow? 'mb-1':'mb-2.5'}`,
+  "bottom": `top-full left-1/2 -translate-x-1/2 ${props.hideArrow? 'mt-1':'mt-2.5'}`
+};
+
+const arrowPositionalClasses = {
+  "left": `border-solid border-y-[${ARROW_SIZE}] border-y-transparent border-l-[${ARROW_SIZE}] border-l-${TOOLTIP_BG_COLOR} left-full top-1/2 -translate-y-1/2`,
+  "right": `border-solid border-y-[${ARROW_SIZE}] border-y-transparent border-r-[${ARROW_SIZE}] border-r-${TOOLTIP_BG_COLOR} right-full top-1/2 -translate-y-1/2`, 
+  "top": `border-solid border-x-[${ARROW_SIZE}] border-x-transparent border-t-[${ARROW_SIZE}] border-t-${TOOLTIP_BG_COLOR} left-1/2 -translate-x-1/2 top-full`,
+  "bottom": `border-solid border-x-[${ARROW_SIZE}] border-x-transparent border-b-[${ARROW_SIZE}] border-b-${TOOLTIP_BG_COLOR} left-1/2 -translate-x-1/2 bottom-full`
+};
+
+const showTooltip = ()=>{
+  isTooltipVisible.value = true;
+}
+const hideTooltip = ()=>{
+  isTooltipVisible.value = false;
+}
+const toggleTooltip= ()=>{
+  isTooltipVisible.value = !isTooltipVisible.value;
+}
 </script>
 
 <style scoped>
-  .tooltip::before,.tooltip::after {
-    --tooltip-bg:v-bind(bgColor);
-    --scale:0;
-    --transition-duration: 100ms;
-    --translate-x: -50%;
-    --opacity: 0;
-    @apply absolute;
-    transform: translateX(var(--translate-x)) translateY(var(--translate-y,0)) scale(var(--scale));
-    opacity: var(--opacity);
-    transition-duration: var(--transition-duration);
-    transition-timing-function: ease-in-out;
-    transition-property: transform , opacity;
-    transform-origin: var(--transition-origin);
-  }
-  .tooltip::before {
-    --translate-y:-100%;
-    content: attr(data-tooltip);
-    @apply w-max max-w-[15rem] text-left px-2.5 py-2.5 font-light text-black text-xs rounded-xl;
-    border:1px solid var(--tooltip-bg);
-    background-color: var(--tooltip-bg);
-  }
-  .tooltip:hover::before,.tooltip:hover::after {
-    --scale:1;
-    --opacity:1;
-  }
-  .tooltip[data-tooltip-position="top"]::before, .tooltip[data-tooltip-position="bottom"]::before {
-    @apply left-1/2;
-  }
-  .tooltip[data-tooltip-position="left"]::before,.tooltip[data-tooltip-position="right"]::before {
-    --translate-y:0%;
-    @apply -top-full;
-  }
-  .tooltip[data-tooltip-position="top"]::before {
-    @apply -top-3;
-    --translate-y:-100%;
-    --transition-origin:bottom center;
-  }
-  .tooltip[data-tooltip-position="bottom"]::before {
-    --translate-y:0%;
-    top:calc(100% + 0.75rem);
-    --transition-origin:top center;
-  }
-  .tooltip[data-tooltip-position="left"]::before {
-    --translate-x:calc(-100% - 0.75rem);
-    @apply left-0;
-    --transition-origin:center right;
-  }
-  .tooltip[data-tooltip-position="right"]::before {
-    --translate-x:0.75rem;
-    @apply left-full;
-    --transition-origin:center left;
-  }
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,32 @@ module.exports = {
   ],
   darkMode: 'media',
   theme: {
-    extend: {},
+    extend: {
+      animation:{
+        fade:"fade 250ms ease-in-out"
+      },
+      keyframes: {
+        fade: {
+          '0%': { opacity:0},
+          '100%': { opacity: 1 },
+        }
+      }
+    },
   },
   variants: {
     extend: {},
   },
+  safelist:[
+    "text-white",
+    ...(['y','x','l','r','t','b'].flatMap((dir)=>`border-${dir}-[0.375rem]`)),
+    {
+      pattern: /border-(l|r|t|b)-(black|blue-700|yellow-500|red-600|neutral-600)/,
+    }, {
+      pattern: /bg-(black|blue-700|yellow-500|red-600|neutral-600)/,
+    }, {
+      pattern: /m(r|l|t|b)-(\d)+/,
+    },
+    
+  ],
   plugins: [],
 };
