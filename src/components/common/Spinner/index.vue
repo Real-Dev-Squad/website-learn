530b3b10213096a9cefcf4ec3927765b358be721
Samarpan Harit <haritsamarpan7@gmail.com>
2022-09-11 00:19:18 +0530

    Added Spinner component

---
+++ src/components/common/Spinner/index.vue
@@ -0,0 +1,35 @@
+<template>
+    <div :class="twMerge(`border-2 border-solid rounded-full animate-spin`,sizeConfig[size],`border-b-${emptyColor} border-r-${emptyColor} border-t-${color} border-l-${color}`)"></div>
+</template>
+
+<script setup>
+import {twMerge} from "tailwind-merge"
+
+defineProps({
+    size:{
+        type:String,
+        default:"md",
+        validator(value){
+            return ["xs","sm","md","lg","xl"].includes(value);
+        }
+    },
+    emptyColor:{
+        type:String,
+        default:"transparent"
+    },
+    color:{
+        type:String,
+        default:"black"
+    }
+})
+const sizeConfig = {
+    "xs":"w-3 h-3",
+    "sm":"w-4 h-4",
+    "md":"w-5 h-5",
+    "lg":"w-7 h-7",
+    "xl":"w-10 h-10"
+}
+</script>
+
+<style scoped>
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,26 @@ module.exports = {
   ],
   darkMode: 'media',
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        spin: {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(360deg)' },
+        }
+      },
+      animation: {
+        'spin': 'spin 700ms linear infinite',
+      }
+    },
   },
+  safelist:[
+    {
+      pattern: /border-(b|r)-(transparent)/,
+    },
+    {
+      pattern: /border-(l|t)-(black)/,
+    }
+  ],
   variants: {
     extend: {},
   },
